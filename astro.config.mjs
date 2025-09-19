7815f33f15295dfb3d0ddda81d646031a3322e7b
Facundo Cordoba <facucordoba200@gmail.com>
2025-09-19 13:24:28 -0300

    added blog and seo things

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://piru.app',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: false,
+    },
+  },
+  experimental: {
+    contentCollectionCache: true,
   },
 });
